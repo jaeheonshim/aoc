99472ed33b68c16e4455343d086ff84efb317103
Jaeheon Shim <git@jaeheonshim.com>
2021-12-04 10:41:00 -0500

    Finish problem 4

--- 2021/4.py
+++ 2021/4.py
@@ -1,54 +1,69 @@
 file = open("4.input")
 lines = file.readlines()
 
-randInput = list(map(int, map(lambda x : x.strip(), lines[0].split(","))))
-
-boardList = []
+drawn_numbers = list(map(int, lines[0].split(",")))
+boards = []
 
 for i in range(2, len(lines), 6):
-    board = []
+    boardRows = []
     for j in range(0, 5):
         row = []
-        for num in lines[i + j].split(" "):
-            if num:
-                row.append(int(num.strip()))
-        board.append(row)
-    boardList.append(board)
+        for str in lines[i + j].split(" "):
+            if str.strip():
+                row.append(int(str.strip()))
+        boardRows.append(row)
+
+    boards.append(boardRows)
 
 def submitInt(num):
-    global boardList
-    for boardIndex in range(0, len(boardList)):
-        for i in range(0, 5):
-            for j in range(0, 5):
-                if boardList[boardIndex][i][j] == num:
-                    boardList[boardIndex][i][j] = -1
+    for board in boards:
+        for row in range(0, 5):
+            for col in range(0, 5):
+                if(board[row][col] == num): 
+                    board[row][col] = -1
 
-def findWinners():
+def returnWinners():
     winners = []
-    for boardIndex in range(0, len(boardList)):
+    for n, board in enumerate(boards):
         for i in range(0, 5):
-            if boardList[boardIndex][i][0] == boardList[boardIndex][i][1] == boardList[boardIndex][i][2] == boardList[boardIndex][i][3] == boardList[boardIndex][i][4] == -1:
-                winners.append(boardIndex)
-                break
-            if boardList[boardIndex][0][i] == boardList[boardIndex][1][i] == boardList[boardIndex][2][i] == boardList[boardIndex][3][i] == boardList[boardIndex][4][i] == -1:
-                winners.append(boardIndex)
-                break
+            if (board[i][0] == board[i][1] == board[i][2] == board[i][3] == board[i][4] == -1) or (board[0][i] == board[1][i] == board[2][i] == board[3][i] == board[4][i] == -1):
+                winners.append((n, board))
 
     return winners
 
-for num in randInput:
-    lastNum = num
-    submitInt(num)
-    winners = findWinners()
-    if(len(winners) > 0):
-        winner = winners[0]
-        break
+def sumboard(board):
+    sum = 0
+    for row in board:
+        for col in row:
+            if col >= 0:
+                sum += col
+    return sum
 
-sum = 0
-winner = boardList[winner]
-for row in winner:
-    for col in row:
-        if col > 0:
-            sum += col
+currentI = 0
+while (len(returnWinners()) == 0):
+    submitInt(drawn_numbers[currentI])
+    currentI += 1
 
-print(sum * lastNum)
+winners = returnWinners()
+print(sumboard(winners[0][1]) * drawn_numbers[currentI - 1])
+boards.pop(winners[0][0])
+
+while currentI < len(drawn_numbers):
+    submitInt(drawn_numbers[currentI])
+    currentI += 1
+
+    winners = returnWinners()
+    breakFlag = False
+    popList = []
+    for winner in winners:
+        if len(boards) > 1:
+            boards[winner[0]] = None
+        else:
+            breakFlag = True
+            break
+
+    boards = list(filter(lambda x: x != None, boards))
+
+    if breakFlag: break
+
+print(sumboard(returnWinners()[0][1]) * drawn_numbers[currentI - 1])
