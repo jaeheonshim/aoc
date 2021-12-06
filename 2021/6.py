625ead6739a872d4893a8a1f899e61ef63c61d52
Jaeheon Shim <git@jaeheonshim.com>
2021-12-06 06:33:23 -0500

    Day 6 part 2

--- 2021/6.py
+++ 2021/6.py
@@ -1,19 +1,38 @@
 file = open("6.input")
 lines = file.readlines()
 input = list(map(int, lines[0].split(",")))
+fishDict = {}
+fishes = []
+
+for fish in input:
+    if fish not in fishDict:
+        fishDict[fish] = 1
+    else:
+        fishDict[fish] = fishDict[fish] + 1
+
+for timer in fishDict:
+    fishes.append([timer, fishDict[timer]])
 
 def tickDay():
-    global input
-    newfish = []
-    for i in range(len(input)):
-        input[i] -= 1
-        if(input[i] < 0):
-            newfish.append(8)
-            input[i] = 6
+    global fishes
 
-    input += newfish
+    newfish = 0
+
+    for fish in fishes:
+        fish[0] -= 1
+        if fish[0] < 0:
+            newfish += fish[1]
+            fish[0] = 6
     
-for i in range(80):
+    fishes.append([8, newfish])
+
+# don't bother reduce lol
+
+for i in range(256):
     tickDay()
 
-print(len(input))
+sum = 0
+for fish in fishes:
+    sum += fish[1]
+
+print(sum)
