61d59234a07264c67e78a43c6502a823a5ff63ad
Jaeheon Shim <git@jaeheonshim.com>
2021-12-11 10:18:49 -0500

    Day 11 part 1

---
+++ 2021/11.py
@@ -0,0 +1,39 @@
+file = open("11.input")
+grid = []
+for line in file:
+    grid.append([int(c) for c in line if c.isnumeric()])
+
+def step():
+    flash_sum = 0
+
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            grid[i][j] += 1
+
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] > 9: flash_sum += flash(i, j)
+
+    for i in range(0, len(grid)):
+        for j in range(0, len(grid[i])):
+            if grid[i][j] == -1: grid[i][j] = 0
+
+    return flash_sum
+
+def flash(r, c):
+    flash_count = 1
+    grid[r][c] = -1
+    for i in range(-1, 2):
+        for j in range(-1, 2):
+            if (r + i) >= len(grid) or (r + i) < 0 or (c + j) >= len(grid[r]) or (c + j) < 0: continue
+            if grid[r + i][j + c] == -1: continue
+            grid[r + i][j + c] += 1
+            if grid[r + i][j + c] > 9: flash_count += flash(r + i, j + c)
+
+    return flash_count
+
+total_flashes = 0
+for i in range(0, 100):
+    total_flashes += step()
+
+print(total_flashes)
