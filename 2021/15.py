e2e9f489b2ac67e1ae429782e15c8ef0279c23d4
Jaeheon Shim <git@jaeheonshim.com>
2021-12-20 18:53:58 -0500

    Finish day 15

--- 2021/15.py
+++ 2021/15.py
@@ -54,3 +54,47 @@ start_node = nodes[0][0]
 end_node = nodes[-1][-1]
 dijkstra(start_node)
 print(end_node.distance)
+
+# PART 2 (unoptimized, takes a bit of time)
+
+def inc(val, n):
+    val += n
+    while val > 9:
+        val = val - 9
+    return val
+
+data.seek(0)
+costs = []
+for line in data:
+    row = []
+    for char in line.strip():
+        row.append(int(char))
+
+    costs.append(row)
+
+newnodes = []
+
+for i in range(len(costs) * 5):
+    newrow = []
+    for j in range(len(costs[i % len(costs)]) * 5):
+        x = i % len(costs)
+        y = j % len(costs[x])
+        n = i // len(costs) + j // len(costs)
+        newrow.append(Node(inc(costs[x][y], n)))
+    newnodes.append(newrow)
+
+for i in range(len(newnodes)):
+    for j in range(len(newnodes[i])):
+        if (i - 1) >= 0:
+            newnodes[i][j].addneighbor(newnodes[i - 1][j])
+        if (j - 1) >= 0:
+            newnodes[i][j].addneighbor(newnodes[i][j - 1])
+        if (i + 1) < len(newnodes):
+            newnodes[i][j].addneighbor(newnodes[i + 1][j])
+        if (j + 1) < len(newnodes[0]):
+            newnodes[i][j].addneighbor(newnodes[i][j + 1])
+
+new_start = newnodes[0][0]
+new_end = newnodes[len(newnodes) - 1][len(newnodes) - 1]
+dijkstra(new_start)
+print(new_end.distance)
